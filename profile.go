bb76cc0c21e47220dc943368c44fd04fa0838e6b
Bakr <bakrbali@gmail.com>
2024-03-19 14:49:07 -0400

    Add pilot box will remain in view at all times
    Getting the form to pop-up was a pain and never worked.

--- profile.go
+++ profile.go
@@ -5,10 +5,6 @@ import (
 	"github.com/rivo/tview"
 )
 
-const (
-	help = `Press [green]Ctrl+N[-] to create a new pilot profile.`
-)
-
 func Profile(nextSlide func()) (title string, content tview.Primitive) {
 	pilotListRoot := tview.NewTreeNode("Pilots").
 		SetSelectable(false).
@@ -18,32 +14,14 @@ func Profile(nextSlide func()) (title string, content tview.Primitive) {
 		SetRoot(pilotListRoot).
 		SetCurrentNode(pilotListRoot)
 
-	frame := tview.NewFrame(tview.NewBox()).
-		AddText(help, true, tview.AlignLeft, tcell.ColorWhite)
+	tree.SetBorder(true)
 
-	pilotForm := tview.NewForm().
-		AddInputField("First Name", "", 20, nil, nil).
-		AddInputField("Last Name", "", 20, nil, nil).
-		AddDropDown("Home Airport", []string{"KBWI", "KFME", "KIAD", "KADW"}, 0, nil).
-		AddButton("Save", nil).
-		AddButton("Quit", func() {
-			app.Stop()
-		})
+	pilotForm := newPilotForm()
 
 	flex := tview.NewFlex().
 		SetDirection(tview.FlexColumn).
-		AddItem(tree, 0, 1, true).
-		AddItem(frame, 0, 1, false).
-		AddItem(pilotForm, 0, 1, true)
-
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Key() == tcell.KeyCtrlR {
-			flex.AddItem(pilotForm, 0, 1, true)
-			app.SetFocus(pilotForm)
-			return nil
-		}
-		return event
-	})
+		AddItem(tree, 1, 1, true).
+		AddItem(pilotForm, 0, 1, false)
 
 	return "Profiles", flex
 }
