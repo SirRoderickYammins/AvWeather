55449cfe83f8de6d0ecea5ba0dc19cf7d18d2677
Bakr <bakrbali@gmail.com>
2024-03-19 14:48:08 -0400

    Added form for new pilot. Basic struct for pilot
    Panic when clicking on button. Form data likely invalid/doesn't exist

--- forms.go
+++ forms.go
@@ -4,15 +4,33 @@ import (
 	"github.com/rivo/tview"
 )
 
+type Pilot struct {
+	FirstName   string
+	LastName    string
+	HomeAirport string
+}
+
+var pilots []Pilot
+
 func newPilotForm() (content tview.Primitive) {
 	form := tview.NewForm().
-		AddInputField("First Name", "", 20, nil, nil).
-		AddInputField("Last Name", "", 20, nil, nil).
-		AddDropDown("Home Airport", []string{"KBWI, KFME, KIAD, KADW"}, 0, nil).
-		AddButton("Save", nil).
-		AddButton("Quit", func() {
-			app.Stop()
-		})
+		AddInputField("First Name", "firstname", 20, nil, nil).
+		AddInputField("Last Name", "lastname", 20, nil, nil).
+		AddDropDown("Home Airport", []string{"KBWI", "KFME", "KIAD", "KADW"}, 0, nil)
 
+	addPilot := func() {
+		selected := form.GetFormItemByLabel("Home Airport").GetLabel()
+
+		thisPilot := Pilot{
+			FirstName:   form.GetFormItemByLabel("firstname").GetLabel(),
+			LastName:    form.GetFormItemByLabel("lastname").GetLabel(),
+			HomeAirport: selected,
+		}
+		pilots = append(pilots, thisPilot)
+	}
+
+	form.AddButton("Add Pilot", addPilot)
+
+	form.SetBorder(true).SetTitle("Add Pilot")
 	return form
 }
